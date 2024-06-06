5462b5ee9328e2f49e4e4046cb0a93851c4e7aee
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-06-06 11:32:05 +0530

    Update SpotifyRepository.java

--- src/main/java/com/driver/SpotifyRepository.java
+++ src/main/java/com/driver/SpotifyRepository.java
@@ -257,94 +257,81 @@ public class SpotifyRepository {
     }
 
     public Song likeSong(String mobile, String songTitle) throws Exception {
-
-        //The user likes the given song. The corresponding artist of the song gets auto-liked
-        //A song can be liked by a user only once. If a user tried to like a song multiple times, do nothing
-        //However, an artist can indirectly have multiple likes from a user, if the user has liked multiple songs of that artist.
-        //If the user does not exist, throw "User does not exist" exception
-        //If the song does not exist, throw "Song does not exist" exception
-        //Return the song after updating
-
-        User user = null;
-        for(User user1:users){
-            if(user1.getMobile()==mobile){
-                user=user1;
-                break;
-            }
-        }
-        if(user==null)
+        // Find the user by mobile number
+        User user = getUserByMobile(mobile);
+        if (user == null) {
             throw new Exception("User does not exist");
-
-        Song song = null;
-        for(Song song1:songs){
-            if(song1.getTitle()==songTitle){
-                song=song1;
-                break;
-            }
         }
-        if (song==null)
+
+        // Find the song by title
+        Song song = getSongByTitle(songTitle);
+        if (song == null) {
             throw new Exception("Song does not exist");
-
-        if(songLikeMap.containsKey(song)){
-            List<User> list = songLikeMap.get(song);
-            if(list.contains(user)){
-                return song;
-            }else {
-                int likes = song.getLikes() + 1;
-                song.setLikes(likes);
-                list.add(user);
-                songLikeMap.put(song,list);
-
-                Album album=null;
-                for(Album album1:albumSongMap.keySet()){
-                    List<Song> songList = albumSongMap.get(album1);
-                    if(songList.contains(song)){
-                        album = album1;
-                        break;
-                    }
-                }
-                Artist artist = null;
-                for(Artist artist1:artistAlbumMap.keySet()){
-                    List<Album> albumList = artistAlbumMap.get(artist1);
-                    if (albumList.contains(album)){
-                        artist = artist1;
-                        break;
-                    }
-                }
-                int likes1 = artist.getLikes() +1;
-                artist.setLikes(likes1);
-                artists.add(artist);
-                return song;
-            }
-        }else {
-            int likes = song.getLikes() + 1;
-            song.setLikes(likes);
-            List<User> list = new ArrayList<>();
-            list.add(user);
-            songLikeMap.put(song,list);
-
-            Album album=null;
-            for(Album album1:albumSongMap.keySet()){
-                List<Song> songList = albumSongMap.get(album1);
-                if(songList.contains(song)){
-                    album = album1;
-                    break;
-                }
-            }
-            Artist artist = null;
-            for(Artist artist1:artistAlbumMap.keySet()){
-                List<Album> albumList = artistAlbumMap.get(artist1);
-                if (albumList.contains(album)){
-                    artist = artist1;
-                    break;
-                }
-            }
-            int likes1 = artist.getLikes() +1;
-            artist.setLikes(likes1);
-            artists.add(artist);
-
-            return song;
         }
+
+        // Check if the user has already liked the song
+        List<User> likedUsers = songLikeMap.getOrDefault(song, new ArrayList<>());
+        if (likedUsers.contains(user)) {
+            return song; // User has already liked the song, do nothing
+        }
+        
+        // Increment the song likes and add the user to the liked users list
+        int likes = song.getLikes() + 1;
+        song.setLikes(likes);
+        likedUsers.add(user);
+        songLikeMap.put(song, likedUsers);
+
+        // Auto-like the corresponding artist
+        Album album = getAlbumOfSong(song);
+        if (album != null) {
+            Artist artist = getArtistOfAlbum(album);
+            if (artist != null) {
+                int artistLikes = artist.getLikes() + 1;
+                artist.setLikes(artistLikes);
+            }
+        }
+    
+        return song;
+        
+    }
+    
+    // Helper method to find a user by mobile number
+    private User getUserByMobile(String mobile) {
+        for (User user : users) {
+            if (user.getMobile().equals(mobile)) {
+                return user;
+            }
+        }
+        return null;
+    }
+    // Helper method to find a song by title
+    private Song getSongByTitle(String title) {
+        for (Song song : songs) {
+            if (song.getTitle().equals(title)) {
+                return song;
+            }
+        }
+        return null;
+    }
+    
+    // Helper method to get the album of a song
+    private Album getAlbumOfSong(Song song) {
+        for (Map.Entry<Album, List<Song>> entry : albumSongMap.entrySet()) {
+            if (entry.getValue().contains(song)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+    
+    // Helper method to get the artist of an album
+    private Artist getArtistOfAlbum(Album album) {
+        for (Map.Entry<Artist, List<Album>> entry : artistAlbumMap.entrySet()) {
+            if (entry.getValue().contains(album)) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 
     public String mostPopularArtist() {
